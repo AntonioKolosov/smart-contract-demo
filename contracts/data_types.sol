ea36a981d6f5f71cc85a6ca7d2747fcdecacdb73
AntonioKolosov <anton.kkolosov@gmail.com>
2024-02-10 17:57:20 +0200

    add new types

--- contracts/data_types.sol
+++ contracts/data_types.sol
@@ -6,12 +6,14 @@ pragma solidity  ^0.8.22;
 
 contract Types {
     // Public variable
-    // boolean. State. Will be store in blockchain
+    // Boolean. State. Will be store in blockchain
     bool public myBool = true; 
     uint public minimum;
     uint public maximum;
     uint public myVal = 254;
-
+    string public myStr = "test"; // Storage string
+    address public myAddr = 0x5B38Da6a701c568545dCfcB03FcB875f56beddC4;
+    mapping (address => uint) public payments;
 
         // Return True if both are True (and)
         // localBool && _inputBool
@@ -24,12 +26,12 @@ contract Types {
         // (not)
         // !localBool
 
-    // unsigned integers
+    // Unsigned integers
     uint256 public myUint = 42; // may text just an uint (only 256 bit) 2 ** 256
     uint8 public mySmallUint = 2; // 2 ** 8 ( 0 --> 255)
     // from uint8 to uint 256
 
-    // signed integers
+    // Signed integers
     int public  myInt = -48; // info about sign storing in 1 bit
     int8 public mySmallInt = -1; // 2 ** 7 = 128 (-128 --> (128-1)
 
@@ -62,5 +64,27 @@ contract Types {
         unchecked {
             myVal++;
         }
+    }
+
+    function str(string memory newValueStr) public {
+        // Tempopary string
+        // Memory str exist until function works
+        myStr = newValueStr;
+    }
+
+    //  Special method balance for address type. For checking balance
+    function getBalance(address targetAddr) public view  returns(uint) {
+        return targetAddr.balance;
+    }
+
+    // Special method transfer for address type. For transfer ether
+    function transferTo(address targetAddr, uint amount) public {
+        address payable _to = payable (targetAddr);
+        _to.transfer(amount);
+    }
+
+    // Get information about sender and value of recieved funds
+    function recieveFunds() public payable {
+        payments[msg.sender] = msg.value;
     }
 }
